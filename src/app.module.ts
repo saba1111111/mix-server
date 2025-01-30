26a69bdca49a8a70f0496de79d89469ecd4eb4ee
Saba Pachulia <sabapachulia@omegas-MacBook-Pro.local>
2025-01-30 08:37:59 +0400

    connecting to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HealthModule } from './health/health.module';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [],
+      synchronize: false,
+    }),
+    TerminusModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
