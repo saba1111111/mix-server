ae5d3c4c592c13ff0282deb9c87f009cd1146693
Saba Pachulia <sabapachulia@omegas-MacBook-Pro.local>
2025-01-30 09:15:13 +0400

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HealthModule } from './health/health.module';
-import { TerminusModule } from '@nestjs/terminus';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
@@ -16,6 +17,10 @@ import { TerminusModule } from '@nestjs/terminus';
       database: process.env.POSTGRES_DATABASE,
       entities: [],
       synchronize: false,
+      ssl:
+        process.env.NODE_ENV === 'development'
+          ? { rejectUnauthorized: false }
+          : true,
     }),
     HealthModule,
   ],
